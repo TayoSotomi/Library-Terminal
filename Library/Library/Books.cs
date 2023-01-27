2b7557a37025f8167543d6088849fb60ac5674ac
Mike <mike.blackburn.541@gmail.com>
2023-01-27 09:21:05 -0500

    added date count, due date, staus, and number generator for ceasar

--- Library/Library/Books.cs
+++ Library/Library/Books.cs
@@ -1,54 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Library
+﻿namespace Library
 {
-    public class Books
+  public class Books
+  {
+    //properties
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Category { get; set; }
+    public bool Status { get; set; }
+    public DateTime Due { get; set; }
+
+    //constructors
+    public Books(string _title, string _author, string _category, bool _status, DateTime _due)
     {
-        //properties
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string Category { get; set; }
-        public bool Status { get; set; }
-        public DateTime Due { get; set; }
-
-        //constructors
-        public Books(string _title, string _author, string _category, bool _status, DateTime _due)
-        {
-            Title = _title;
-            Author = _author;
-            Category = _category;
-            Status = _status;
-            Due = _due;
-        }
-        //method
-        public string GetInfo()
-        {
-            return ($"{Title}|{Author}|{Category}|{Status}|{Due}");
-        }
-        public DateTime GetDueDate(DateTime receivedDatedTime)
-        {
-            DateTime checkOutDate = receivedDatedTime;
-            DateTime dueDate = checkOutDate.AddDays(13);
-            return dueDate;
-        }
-
-        public bool GetStatus()
-        {
-            return Status;
-        }
+      Title = _title;
+      Author = _author;
+      Category = _category;
+      Status = _status;
+      Due = _due;
     }
+    //method
+    public string GetInfo()
+    {
+      return ($"{Title}|{Author}|{Category}|{Status}|{Due}");
+    }
+    public DateTime GetDueDate(DateTime checkOutDate)
+    {
+      DateTime check = checkOutDate;
+      DateTime dueDate = checkOutDate.AddDays(13);
+      return Due;
+    }
+    public double GetDateCount(DateTime dueDate, DateTime checkOutDate)
+    {
+      DateTime due = dueDate;
+      DateTime check = checkOutDate;
+      dueDate.Subtract(checkOutDate);
+      var c = due.Subtract(check);
+      double x = c.Days;
+      return x; //returned variable will be called date count and an int
+    }
+
+    public bool GetStatus()
+    {
+      return Status;
+    }
+    public static int GetRandomEditMinMax(int x, int min, int max)
+    {
+      Random r = new Random();
+      return r.Next(min, max);
+    }
+  }
 }
-        
 
-      
 
-      
-        
-    
 
-    
+
+
+
+
+
+
 
